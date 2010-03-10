27cac2f54800ef85c424e9cd31f6253502cab7ab
valentin.gosu <valentin.gosu@3b8d4516-26f2-11df-a76c-adfa62dee608>
2010-03-10 22:59:58 +0000

    Xbrd.java // still a draft. can't get it tot work properly

--- branches/vali/XBoard.java
+++ branches/vali/XBoard.java
@@ -8,35 +8,36 @@ import java.io.InputStreamReader;
 public class XBoard{
 	BufferedReader inReader=null;
 	BufferedWriter log=null; // log file
-	
+
 	public static int WHITE = 0;
-    public static int BLACK = 1;
-    
-    int yourColor;
-    int toMove;
-	
-    String lMove;
-    
-    int time;
-    int otim;
-    
+	public static int BLACK = 1;
+	public static int FORCE = -1;
+
+	public int yourColor = FORCE;
+	public int toMove = WHITE;
+
+	public String lMove = "";
+
+	public int time;
+	public int otim;
+
 	public XBoard(String outf) throws Exception{
 		inReader = new BufferedReader(new InputStreamReader(System.in));
 		log = new BufferedWriter(new FileWriter(outf));
 	}
-	
+
 	public XBoard() throws Exception{
 		inReader = new BufferedReader(new InputStreamReader(System.in));
 		log = new BufferedWriter(new FileWriter("out.txt"));
 	}
-	
+
 	public XBoard(boolean printLogs) throws Exception{
 		inReader = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		if (printLogs)
-			log = new BufferedWriter(new FileWriter("out.txt"));
+			log = new BufferedWriter(new FileWriter("D:/out.txt"));
 	}
-	
+
 	// initializeaza conexiunea
 	// se asteapta mutarea
 	public void init() throws Exception {
@@ -45,12 +46,12 @@ public class XBoard{
 		// init time
 		// totul pana la GO
 		String read="";
-		while (System.in.available()!=0){
+		while (!read.contains("go")){
 			read=inReader.readLine();
-			
+
 			if (log!=null)
 				log.write(read+"\n");
-			
+
 			if (read.contains("xboard"))
 				;
 			if (read.contains("protover"))
@@ -63,53 +64,69 @@ public class XBoard{
 				yourColor=WHITE;
 			if (read.contains("black"))
 				yourColor=BLACK;
-			if (read.contains("usermove") && !read.contains("accepted"))
-				; // you are white/computer black - set lMoves
-			if (read.contains("go"))
-				return ; // make move; init() stops  -> break;
+			if (read.contains("usermove") && !read.contains("accepted")) {
+				lMove = read.substring(8);
+				yourColor = BLACK;
+				toMove = BLACK;
+			} // you are white/computer black - set lMoves
+			if (read.contains("go")) {
+				toMove = yourColor;
+				return ;
+			}// make move; init() stops  -> break;
 			if (read.contains("force"))
-				; // ??
+				yourColor = FORCE; // ??
+			if (read.contains("otim"))
+				otim = Integer.parseInt(read.substring(4));
+			if (read.contains("otim"))
+				otim = Integer.parseInt(read.substring(4));
 		}
+		close();
+		write("quit");
 	}
+
 	public void close() throws Exception {
 		if (log!=null)
 			log.close();
-		
+
 	}
-	
+
 	public String lastMove() {
 		return lMove;
 	}
-	
+
 	public void write(String command) throws Exception{
 		System.out.println(command);
 		if (log!=null)
 			log.write("_"+command+"\n");
+		if (command.contains("move"))
+			toMove = (toMove+1)&1;
 	}
-	
+
 	public void readAndLog() throws Exception {
 		String read="";
-		while ( System.in.available() !=0 ) {
+		while ( System.in.available() > 0 ) {
 			read=inReader.readLine();
+
 			if (log!=null)
 				log.write(read+"\n");
-			
+
 			if (read.contains("usermove")) {
 				lMove=read.substring(8); // ??
+				toMove = (toMove+1)&1;
 			}
 			if (read.equals("quit")){
-	    		;
-	    	}
-	    	// ce face??
-	    	if (read.equals("force")){
-	    		;
-	    	}
-	    	if (read.contains("time")){
-	    		time = Integer.parseInt(read.substring(8));//
-	    	}
-	    	if (read.contains("otim")){
-	    		otim = Integer.parseInt(read.substring(8));//
-	    	}
+				close();
+			}
+			// ce face??
+			if (read.equals("force")){
+				;
+			}
+			if (read.contains("time")){
+				time = Integer.parseInt(read.substring(4));//
+			}
+			if (read.contains("otim")){
+				otim = Integer.parseInt(read.substring(4));//
+			}
 		}
 	}
 }
