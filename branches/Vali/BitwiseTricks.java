8f903657e02cf4cf25cd86d5042d3885d4c51465
valentin.gosu <valentin.gosu@3b8d4516-26f2-11df-a76c-adfa62dee608>
2010-05-12 20:55:06 +0000

    Sortarea mutarilor dupa piesa luata, apoi dupa tipul piesei. (sper ca merge :) ) Modificat in Game, daca dam ca argument "it" sa faca iterative deepening, altfel face AlfaBeta :D

--- branches/Vali/BitwiseTricks.java
+++ branches/Vali/BitwiseTricks.java
@@ -25,15 +25,5 @@ public class BitwiseTricks {
 		return index64[(int) ((mask * debruijn64 ) >>> 58)];
 	}
 	
-	public static long highestOneBit(long i)
-	{
-	  i |= (i >> 1);
-	  i |= (i >> 2);
-	  i |= (i >> 4);
-	  i |= (i >> 8);
-	  i |= (i >> 16);
-	  i |= (i >> 32);
-	  return i - (i >>> 1);
-	}
-
+	
 }
--- branches/Vali/Game.java
+++ branches/Vali/Game.java
@@ -8,42 +8,65 @@ import java.io.*;
  */
 
 public class Game {
-    public static Board brd=new Board();
-    
-    
-    public static void main(String []args) throws Exception{
-       	XBoard xboard = new XBoard();
-       	Board.generateStatic();
-        Openings.reset();
-        while(xboard.isAlive()) {       	
-        	if(xboard.isTurn()) {
-        		if (!Openings.isSet())
-        			Openings.init(0);
-            	xboard.sendToXBoard(brd.nextMove((byte)xboard.side));
-            }
-        	boolean citit = xboard.read();
-            if (xboard.lastMove.startsWith("usermove") && xboard.side!=xboard.engine){
-            		if (!Openings.isSet())
-            			Openings.init(1);
-                	String mutare = xboard.lastMove.substring(9);
-            		brd.SAN(mutare,xboard.side);					
-            		brd.updateBoard(brd.pos1,brd.pos2,brd.promotion);	
-            		xboard.chSide();
-            		Openings.makeMove(new Move(brd.pos1,brd.pos2));
-            		//prelucrare timpi
-            		xboard.time = 0;
-            		xboard.otim = 0;
-                }     
-            else if (xboard.lastMove.startsWith("new")) {
-            		brd = new Board();
-            		Openings.reset();
-            }
-            else if (xboard.lastMove.contains("printboard")){
-            	brd.printBoard();
-            	//System.out.println("#"+Openings.getMove().toString()+"\n");
-            }
-        }
-    }
+	public static Board brd=new Board();
+
+
+	public static void main(String []args) throws Exception{
+		XBoard xboard = new XBoard();
+		Board.generateStatic();
+		Openings.reset();
+		if (args.length==0) {
+			while(xboard.isAlive()) {       	
+				if(xboard.isTurn()) {
+					if (!Openings.isSet())
+						Openings.init(0);
+					xboard.sendToXBoard(brd.nextMove((byte)xboard.side));
+				}
+				boolean citit = xboard.read();
+				if (xboard.lastMove.startsWith("usermove") && xboard.side!=xboard.engine){
+					if (!Openings.isSet())
+						Openings.init(1);
+					String mutare = xboard.lastMove.substring(9);
+					brd.SAN(mutare,xboard.side);					
+					brd.updateBoard(brd.pos1,brd.pos2,brd.promotion);	
+					xboard.chSide();
+					Openings.makeMove(new Move(brd.pos1,brd.pos2));
+				}     
+				else if (xboard.lastMove.startsWith("new")) {
+					brd = new Board();
+					Openings.reset();
+				}
+				else if (xboard.lastMove.contains("printboard")){
+					brd.printBoard();
+					//System.out.println("#"+Openings.getMove().toString()+"\n");
+				}
+			}
+		} else if (args[0].contains("it")) {
+			while(xboard.isAlive()) {       	
+				if(xboard.isTurn()) {
+					if (!Openings.isSet())
+						Openings.init(0);
+					xboard.sendToXBoard(brd.nextMove((byte)xboard.side,xboard.time,xboard.otim));
+				}
+				boolean citit = xboard.read();
+				if (xboard.lastMove.startsWith("usermove") && xboard.side!=xboard.engine){
+					if (!Openings.isSet())
+						Openings.init(1);
+					String mutare = xboard.lastMove.substring(9);
+					brd.SAN(mutare,xboard.side);					
+					brd.updateBoard(brd.pos1,brd.pos2,brd.promotion);	
+					xboard.chSide();
+					Openings.makeMove(new Move(brd.pos1,brd.pos2));
+				}     
+				else if (xboard.lastMove.startsWith("new")) {
+					brd = new Board();
+					Openings.reset();
+				}
+				else if (xboard.lastMove.contains("printboard")){
+					brd.printBoard();
+				}
+			}
+		}
+	}
 }
-    
- 
+
